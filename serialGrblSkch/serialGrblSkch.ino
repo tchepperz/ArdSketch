4b5dc40bcfcfb68ef6dba28bc0ea3591807fd3a9
tchepperz <p_giusti@hotmail.fr>
2015-03-19 23:34:43 +0100

    grbl serial sketch
    Drive grbl via serial port from Mega

---
+++ serialGrblSkch/serialGrblSkch.ino
@@ -0,0 +1,180 @@
+/*Here is a little arduino sketch which
+open a gcode file from sd card reader connected to arduino
+mega and stream it via Serial to an arduino uno which is 
+running grbl. You need to launch serial
+monitor to interact with the program.
+You can replace the serial monitor interface by any
+LCD touchscreen interface and then you have CNC without any 
+computer.
+Enjoy!*/
+
+#include <SD.h>
+
+File myFile;
+boolean restart = false;		
+
+void setup(){
+	Serial.begin(115200);
+	Serial1.begin(115200);
+
+}
+
+
+void loop(){
+
+	checkSD();
+	while(!restart){
+		openFileSD();
+		sendGcode();
+	}
+}
+
+
+
+void checkSD(){
+  // Check if SD card is OK
+  // On the Ethernet Shield, CS is pin 4. It's set as an output by default.
+  // Note that even if it's not used as the CS pin, the hardware SS pin 
+  // (10 on most Arduino boards, 53 on the Mega) must be left as an output 
+  // or the SD library functions will not work.
+  while(!SD.begin(53))		// change this to 10 on uno
+  {
+    Serial.println("Please insert SD card...\n");
+    delay(1000);
+  }
+   
+  Serial.println("SD card OK...\n");
+  delay(1000);
+}
+
+void openFileSD(){
+
+	String fileName = "";
+	char fileNameChar[100]={0};		// char array for SD functions arguments
+
+	Serial.println("Enter name for a gcode file on SD : \n");
+	emptySerialBuf(0);
+	fileName=getSerial(0);
+
+	for(int i=0;fileName.charAt(i)!='\n';i++)		//convert String in char array removing '\n'
+		fileNameChar[i]=fileName.charAt(i);
+
+	if(!SD.exists(fileNameChar)){					//check if file elready exists
+		Serial.print("-- ");
+		Serial.print(fileNameChar);
+		Serial.print(" doesn't exists");
+		Serial.println(" --");
+		Serial.println("Please select another file\n");
+		
+      	delay(200);
+      	openFileSD();
+
+    } 
+    else{
+     	myFile = SD.open(fileNameChar, FILE_READ);		//create a new file
+     	Serial.print("-- ");
+     	Serial.print("File : ");
+		Serial.print(fileNameChar);
+		Serial.print(" opened!");
+		Serial.println(" --\n");
+    }
+}
+
+void emptySerialBuf(int serialNum){
+	//Empty Serial buffer, argument serialNum for Serial port number
+	if(serialNum==0){
+	 	while(Serial.available())                      
+			Serial.read();
+	}
+	else if(serialNum==1){
+		while(Serial1.available())                      
+    		Serial1.read();
+    }
+}
+
+void waitSerial(int serialNum){
+	// Wait for data on Serial, argument serialNum for Serial port number
+ 	boolean serialAv = false;
+
+ 	if(serialNum==0){
+ 		while(!serialAv){ 
+			if(Serial.available())
+     		serialAv=true;
+ 		}
+	}
+	else if(serialNum==1){
+		while(!serialAv){ 
+			if(Serial1.available())
+			serialAv=true;
+		}
+	}
+}
+
+String getSerial(int serialNum){
+	//Return String  from serial line reading, argument serialNum for Serial port number
+
+	String inLine = "";
+	waitSerial(serialNum);
+	if(serialNum==0){
+		while(Serial.available()){              
+			inLine += (char)Serial.read();
+			delay(2);
+		}
+		return inLine;
+	}
+
+	else if(serialNum==1){
+		while(Serial1.available()){               
+    		inLine += (char)Serial1.read();
+    		delay(2);
+ 		}
+	return inLine;
+	}
+}
+
+void sendGcode(){
+	/*READING GCODE FILE AND SEND ON SERIAL PORT TO GRBL*/
+
+	//START GCODE SENDING PROTOCOL ON SERIAL 1
+
+	String line = "";
+
+    Serial1.print("\r\n\r\n");                           //Wake up grbl
+    delay(2);
+    emptySerialBuf(1);
+    if(myFile){                                                                      
+	    while(myFile.available()){          				//until the file's end
+	    	line = readLine(myFile);                        //read line in gcode file 
+	      	Serial.print(line);                             //send to serials
+	      	Serial1.print(line);
+	      	Serial.print(getSerial(1));						//print grbl return on serial
+		}
+	}
+	else
+		fileError();
+
+	myFile.close();
+	Serial.println("Finish!!\n");
+	delay(2000);
+}
+
+void fileError(){
+	// For file open or read error
+	Serial.println("\n");
+	Serial.println("File Error !");
+	restart = true;
+}
+
+String readLine(File f){
+	//return line from file reading
+  
+	char inChar;
+	String line = "";
+
+	do{
+		inChar =(char)f.read();
+    	line += inChar;
+    }while(inChar != '\n');
+
+	return line;
+}
